2b6a6e67f425ea4167af348809d14f03264fa665
YorchMx <al02712191@tecmilenio.mx>
2020-11-22 21:50:13 -0600

    Clase conexion java

---
+++ src/main/java/datos/Conexion.java
@@ -0,0 +1,49 @@
+package datos;
+
+import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class Conexion {
+    private static final String JDBC URL = "jdbc:mysql://localhost:3306/control_ventas?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC USER = "root";
+    private static final String JDBC PASSWORD = "Abrelata$7";
+    
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASSWORD);
+        ds.setInitialSize(50);
+        return ds;
+    }
+    
+    public static Connection getConection() throws SQLException{
+        return getDataSource().getConnectio();
+    }
+    
+    public static void close(ResultSet rs){
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        
+    }
+    
+    public static void close(PreparedStatement stmt){
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
