fa2c5ff1bb97eac78c2471aa42b976eb3757f358
YorchMx <al02712191@tecmilenio.mx>
2020-11-23 00:18:25 -0600

    Corrección en clase conexion java

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -5,9 +5,9 @@ import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Conexion {
-    private static final String JDBC URL = "jdbc:mysql://localhost:3306/control_ventas?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-    private static final String JDBC USER = "root";
-    private static final String JDBC PASSWORD = "Abrelata$7";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/control_ventas?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "Abrelata$7";
     
     public static DataSource getDataSource(){
         BasicDataSource ds = new BasicDataSource();
